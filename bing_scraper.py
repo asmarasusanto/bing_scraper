2d3f263e042abf2f5f6b5cede1829804e54f4e35
Gary Roberts <Gary Roberts>
2017-03-02 13:20:47 -0800

    Added debug print

--- bing_scraper.py
+++ bing_scraper.py
@@ -44,6 +44,7 @@ def write_imgs(imgs, dest):
         try:
             r = requests.get(img.thumbnail_url, stream=True)
             path = dest + str(i) + '.' + img.encoding_format
+            print('Writing: ' + path)
             if r.status_code == 200:
                 # Copy to file in chunks
                 with open(path, 'wb') as f:
